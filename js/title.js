3c03c76d837ac5b51afbb56cee5b3bd83b4421fe
Lyanp <1589878520@qq.com>
2022-09-29 03:50:05 +0800

    Site updated: 2022-09-29 03:50:04

--- js/title.js
+++ js/title.js
@@ -1,1 +1,17 @@
-"use strict";var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="喂，你有在偷看罢",clearTimeout(titleTime)):(document.title="嗨嗨害，来辣",titleTime=setTimeout(function(){document.title=OriginTitle},1e3))});
+//动态标题
+const OriginTitle = document.title;
+let titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '喂，你有在偷看罢';
+        clearTimeout(titleTime);
+    } else {
+        //返回当前页面时标签显示内容
+        document.title = '嗨嗨害，来辣';
+        //变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 1000);
+    }
+});
